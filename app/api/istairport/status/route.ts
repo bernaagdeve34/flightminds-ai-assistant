053de7f2c8736a12b1cb13ee5fb549a5c8eac44f
Berna Agdeve <agdeveberna1@gmail.com>
2025-10-21 08:50:39 +0300

    canlı veri hatası düzeltildi

--- app/api/istairport/status/route.ts
+++ app/api/istairport/status/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 const ORIGIN = "https://www.istairport.com/umbraco/api/FlightInfo/GetFlightStatusBoard";
 
 export async function GET(req: NextRequest) {
